b32a3fa592894e9cc84ae18c0cbd0b7b60605472
Jenhao <jenhao.hsiao@gmail.com>
2021-10-27 23:20:29 -0400

    Added Logo and icon

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* https://api.rawg.io/docs/ */}
+        {/* API Key */}
+        {/* 04f19512cf744444b13c42bd2f1026d4 */}
       </header>
     </div>
   );
